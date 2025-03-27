29eff981fc0dc9804416c18f86f49590aa7964b8
Ravishankar Ravindran <raviravindran@Ravishankars-MacBook-Pro.local>
2025-03-27 13:39:10 -0700

    removed files

--- test.py
+++
@@ -1,8 +0,0 @@
-#write a function to calculate the factorial of a number
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-print(factorial(5))
-print(factorial(10))
